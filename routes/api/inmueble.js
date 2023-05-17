03fa65614ffabcb58d19b50768d880cbac0b63d2
vdiezace <victoriadiezacereda@gmail.com>
2023-05-17 21:49:15 +0100

    Adding put, post and delete functions

--- routes/api/inmueble.js
+++ routes/api/inmueble.js
@@ -4,4 +4,16 @@ router.get("/", (req, res) => {
   res.send("Prueba");
 });
 
+router.post("/", (req, res) => {
+  res.send("Otra Prueba");
+});
+
+router.put("/:inmuebleId", (req, res) => {
+  res.send("Actualiza");
+});
+
+router.delete("/:inmuebleId", (req, res) => {
+  res.send("Borra");
+});
+
 module.exports = router;
