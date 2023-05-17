fc5583fdc1208a145887470906cf2e1ff76643f9
vdiezace <victoriadiezacereda@gmail.com>
2023-05-17 22:13:04 +0100

    creating the model

--- model/inmueble.model.js
+++ model/inmueble.model.js
@@ -1,1 +1,13 @@
 const { model, Schema } = require("mongoose");
+
+const inmuebleSchema = new Schema({
+  floor: Number,
+  letter: String,
+  extension: Number,
+  number_rooms: Number,
+  rented: Boolean,
+  owner_name: String,
+  contact_email: String,
+});
+
+module.exports = model("inmueble", inmuebleSchema);
